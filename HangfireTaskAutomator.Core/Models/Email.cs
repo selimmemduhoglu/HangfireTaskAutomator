7b1dbd28b3dfc3b38fa81e726d0af24ea3a89272
selim.memduhoglu <selim.memduhoglu@treasy.com>
2025-04-10 02:00:04 +0300

    added :  scheme folders and file added

---
+++ HangfireTaskAutomator.Core/Models/Email.cs
@@ -0,0 +1,12 @@
+﻿namespace HangfireTaskAutomator.Core.Models;
+
+public class Email
+{
+    public int Id { get; set; }
+    public string Recipient { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+    public bool IsSent { get; set; }
+    public DateTime? SentAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
